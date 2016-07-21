397b16f4b3d0a1223e6202982f8de5d0a3997129
sidsenkumar11 <pikahero2@gmail.com>
2016-07-21 08:33:40 -0400

    Successfully queries API with track name and artist name. Information is also already parsed into a dictionary.

--- Tagger.py
+++ Tagger.py
@@ -4,11 +4,6 @@ import json
 clientID = '420737233-ABB8789B14C3A2BAE6730A0EEE59B3D6'
 userID = '94671866235528590-B0AC2AB3FE6629CD0B956F996F3D926A'
 
-print('\nSearch for artist "Kings of Convenience"\n')
-result = pygn.search(clientID=clientID, userID=userID, artist="Miley Cyrus")
-print(result)
-print(json.dumps(result, sort_keys=True, indent=4))
-
 # Rachel
 def read_files_in_current_directory():
 
@@ -18,26 +13,35 @@ def read_files_in_current_directory():
 # Sid
 def get_song_metadata(song_name, artist_name):
 
-	# Query database and get song metadata for the given song
-	return None
+	# Query database and get song metadata as a dict
+	# How to query the dict can be seen here:
+	# https://github.com/cweichen/pygn
+	metadata = pygn.search(clientID=clientID, userID=userID, artist=artist_name, track=song_name)
+	# To see the metadata printed out, uncomment the line below
+	# print(json.dumps(result, sort_keys=True, indent=4))
+	return metadata
 
 # Rahul
 def parse_metadata_from_json(metadata):
-
-	# Get relevant pieces of information from metadata and return dict
+	## UPDATE: turns out this function isn't really needed anymore b/c our API is pretty nice.
+	# Take in dict created by the API from querying the track name and artist name
+	# Get relevant pieces of information that go into MP3 files and return dict containing mapping
 	return None
 
 # Daiven
 def write_metadata_to_file(metadata, MP3_file):
 
 	# Doesn't need to return anything
+	pass # Remove this line when the method is implemented
 
 # Whoever
 def generate_playlists(option):
 
 	# Put each song into folders based on the playlist option
 	# Doesn't need to return anything
+	pass # Remove this line when the method is implemented
 
 if __name__=='__main__':
 	# GUI Code - Priya
+	print(get_song_metadata('Lose Yourself', 'Eminem')['album_art_url'])
 
