a1e25e93e3a93a1e06a49413dafac85054cff612
sidsenkumar11 <pikahero2@gmail.com>
2016-07-25 10:18:27 -0400

    Read and write metadata to file completed. Needs testing. Uses mutagen package

--- Tagger.py
+++ Tagger.py
@@ -3,7 +3,11 @@ import json
 import sys
 import design
 import os
+from mutagen.mp3 import MP3
+from mutagen.easyid3 import EasyID3
+import mutagen.id3
 from PyQt4 import QtCore, QtGui
+import pprint as pp
 
 clientID = '420737233-ABB8789B14C3A2BAE6730A0EEE59B3D6'
 userID = '94671866235528590-B0AC2AB3FE6629CD0B956F996F3D926A'
@@ -11,39 +15,42 @@ userID = '94671866235528590-B0AC2AB3FE6629CD0B956F996F3D926A'
 # Rachel
 def read_files_in_current_directory(dir_name):
 
-	# Return list of MP3 Files in current directory
-	return None
+    # Return list of MP3 Files in current directory
+    return None
 
 # Sid
 def get_song_metadata(song_name, artist_name):
 
-	# Query database and get song metadata as a dict
-	# How to query the dict can be seen here:
-	# https://github.com/cweichen/pygn
-	metadata = pygn.search(clientID=clientID, userID=userID, artist=artist_name, track=song_name)
-	# To see the metadata printed out, uncomment the line below
-	# print(json.dumps(result, sort_keys=True, indent=4))
-	return metadata
-
-# Rahul
-def parse_metadata_from_json(metadata):
-	## UPDATE: turns out this function isn't really needed anymore b/c our API is pretty nice.
-	# Take in dict created by the API from querying the track name and artist name
-	# Get relevant pieces of information that go into MP3 files and return dict containing mapping
-	return None
+    # Query database and get song metadata as a dict
+    # How to query the dict can be seen here:
+    # https://github.com/cweichen/pygn
+    metadata = pygn.search(clientID=clientID, userID=userID, artist=artist_name, track=song_name)
+    # To see the metadata printed out, uncomment the line below
+    # print(json.dumps(result, sort_keys=True, indent=4))
+    return metadata
 
 # Daiven
-def write_metadata_to_file(metadata, MP3_file):
+def write_metadata_to_file(metadict, filename):
 
-	# Doesn't need to return anything
-	pass # Remove this line when the method is implemented
+    mp3file = MP3(filename, ID3=EasyID3)
+    mp3file['Title'] = metadict["track_title"]
+    mp3file['Artist']= metadict["album_artist_name"]
+    mp3file['Album']= metadict["album_title"]
+    mp3file['TrackNumber'] = metadict["track_number"]
+    mp3file['Genre'] = metadict['genre']['1']['TEXT'] # TODO: Write multiple genres
+    mp3file['Date'] = metadict["album_year"]
+    mp3file.save()
+
+def read_metadata_from_file(filename):
+    audio = MP3(filename, ID3=EasyID3)
+    pp.pprint(audio)
 
 # Whoever
 def generate_playlists(option):
 
-	# Put each song into folders based on the playlist option
-	# Doesn't need to return anything
-	pass # Remove this line when the method is implemented
+    # Put each song into folders based on the playlist option
+    # Doesn't need to return anything
+    pass # Remove this line when the method is implemented
 
 class App(QtGui.QMainWindow, design.Ui_MainWindow):
     def __init__(self, parent=None):
@@ -53,16 +60,19 @@ class App(QtGui.QMainWindow, design.Ui_MainWindow):
     def browse_folder(self):
         self.dir_list.clear()
         directory = QtGui.QFileDialog.getExistingDirectory(self,
-        	"Pick a folder")
+            "Pick a folder")
         if directory:
             for file_name in os.listdir(directory):
                 metadata_popup
-	def metadata_popup():
+    def metadata_popup():
+        pass
 
 if __name__=='__main__':
-	# GUI Code - Priya
-	app = QtGui.QApplication(sys.argv)
-	form = App()
-	form.show()
-	app.exec_()
-	print(get_song_metadata('Lose Yourself', 'Eminem')['album_art_url'])
+    # GUI Code - Priya
+    # app = QtGui.QApplication(sys.argv)
+    # form = App()
+    # form.show()
+    # app.exec_()
+    # song_metadata = get_song_metadata("NYC", 'Kevin Rudolph')
+    # write_metadata_to_file(song_metadata, "test.mp3")
+    read_metadata_from_file("test.mp3")
